af204c03d58a8014a3e98d9cfb0220aff30f42a4
Admin <admin@example.com>
2023-10-11 07:44:13 +0100

    Read file

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-// const fs = require("fs");
 import fs from "fs";
+
+fs.writeFile("example.txt", "hello world!!!", (err) => {
+    if (err) throw err;
+    console.log("Example file created.");
+
+    fs.readFile("example.txt", 'utf-8', (err, data) => {
+        if (err) throw err;
+        console.log("File contents:");
+        console.log(data);
+    })
+
+})
+
+// if no error in code then err value will be null
+// method((err, data)=>{
+
+// })
