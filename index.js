80dd5d975dff11365f21e321b37abae437b75859
Admin <admin@example.com>
2023-10-11 08:34:59 +0100

    Added node modules for Chalk

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { DemoHandler as FSDemoHandler, DemoHandler2 } from "./demo/demo-fs.js"
 import { DemoHandler as PathDemoHandler } from "./demo/demo-path.js";
 import "./demo/demo-http.js";
+import "./demo/demo-os.js";
 
 // FSDemoHandler();
 // DemoHandler2();
