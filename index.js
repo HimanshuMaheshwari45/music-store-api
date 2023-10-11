97f2f36808263c991668478954a0c2be45263ebf
Admin <admin@example.com>
2023-10-11 08:12:04 +0100

    Added HTTP server.

--- index.js
+++ index.js
@@ -1,33 +1,7 @@
-import fs from "fs";
+import { DemoHandler as FSDemoHandler, DemoHandler2 } from "./demo/demo-fs.js"
+import { DemoHandler as PathDemoHandler } from "./demo/demo-path.js";
+import "./demo/demo-http.js";
 
-fs.writeFile("example.txt", "hello world!!!", (err) => {
-    if (err) throw err;
-    console.log("Example file created.");
-
-    // fs.readFile("example.txt", 'utf-8', (err, data) => {
-    //     if (err) throw err;
-    //     console.log("File contents:");
-    //     console.log(data);
-    // })
-
-    fs.appendFile("example.txt", "added new content for demo", 'utf8', (err) => {
-        if (err) throw err;
-
-        fs.readFile("example.txt", 'utf-8', (err, data) => {
-            if (err) throw err;
-            console.log("File contents:");
-            console.log(data);
-
-            fs.unlink("example.txt", (err) => {
-                if (err) throw err;
-                console.log("File deleted successfully.")
-            })
-        })
-    })
-
-})
-
-// if no error in code then err value will be null
-// method((err, data)=>{
-
-// })
+// FSDemoHandler();
+// DemoHandler2();
+// PathDemoHandler();
