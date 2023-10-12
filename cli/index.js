49bd53fe5a346be8d5d854bf66b45588dd6aa3e3
Admin <admin@example.com>
2023-10-12 06:53:43 +0100

    Cli with async await

--- cli/index.js
+++ cli/index.js
@@ -1,4 +1,4 @@
-import fs from "fs";
+import fs from "fs/promises";
 import path from "path";
 
 const filepath = "phonebook.json";
@@ -6,14 +6,32 @@ const filepath = "phonebook.json";
 const [_node, _bin, cmd, ...params] = process.argv;
 const [name, phoneNumber] = params;
 
+
+async function addContact() {
+
+    let contacts = []
+    try {
+        const filedata = await fs.readFile(filepath, 'utf8')
+        contacts = JSON.parse(filedata);
+    } catch (error) {
+        console.log("phonbook file didn't exists.")
+    }
+
+    contacts.push({ name, phoneNumber })
+    await fs.writeFile(filepath, JSON.stringify(contacts));
+
+    console.log("New contact added.")
+
+}
+
+
 switch (cmd) {
-    case 'add':
-        if (fs.exists(filepath, (data) => {
-            const contacts = [{ name, phoneNumber }]
-            fs.writeFile(filepath, JSON.stringify(contacts), ()=>{
-                console.log("New contact added");
-            })
-        }))
+    case "add":
+        addContact()
+        break;
+
+    case "search":
+
         break;
 
     default:
