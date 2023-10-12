cf9821587e350f5fafda41071320e6029fc65fd0
Admin <admin@example.com>
2023-10-12 07:43:04 +0100

    updated the cli with yargs

--- cli/index.js
+++ cli/index.js
@@ -1,13 +1,11 @@
 import fs from "fs/promises";
 import path from "path";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers"
 
 const filepath = "phonebook.json";
 
-const [_node, _bin, cmd, ...params] = process.argv;
-const [name, phoneNumber] = params;
-
-
-async function addContact() {
+async function addContact(name, phoneNumber) {
 
     let contacts = []
     try {
@@ -36,16 +34,52 @@ async function listContacts() {
     }
 }
 
+const argv = yargs(hideBin(process.argv))
+    .command('add', "Add new contact", {
+        name: {
+            description: "Contact Name",
+            alias: "n",
+            type: "string",
+            demandOption: true
+        },
+        number: {
+            description: "Contact Number",
+            alias: "m",
+            type: "string",
+            demandOption: true
+        }
+    })
+    .command('list', "List all contacts")
+    .help()
+    .alias('help', 'h')
+    .argv;
 
-switch (cmd) {
-    case "add":
-        addContact()
-        break;
-
-    case "list":
-        listContacts();
-        break;
-
-    default:
-        break;
+async function run() {
+    if (argv._.includes('add')) {
+        await addContact(argv.name, argv.number)
+    } else if (argv._.includes('list')) {
+        await listContacts();
+    } else {
+        console.log("Invalid command")
+    }
 }
+
+run();
+
+
+// switch (cmd) {
+//     case "add":
+//         addContact()
+//         break;
+
+//     case "list":
+//         listContacts();
+//         break;
+
+//     default:
+//         break;
+// }
+
+// cli add abc 232424
+
+// cli add --name abc --email abc@abc.com --number 489024982423
