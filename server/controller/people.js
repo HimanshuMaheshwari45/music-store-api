78f7988e8f86de597f3cf2d7ddb940c582418b0e
Admin <admin@example.com>
2023-10-13 08:36:00 +0100

    Added try catch.

--- server/controller/people.js
+++ server/controller/people.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
 export async function getPeople(req, res) {
-    const { data } = await axios.get("https://jsonplaceholder.typicode.com/users")
-    res.json(data);
+    try {
+        const { data } = await axios.get("https://jsonplaceholder.typicode.com/users")
+        res.json(data);
+    } catch (error) {
+        res.json({ message: "Internal Error." })
+    }
 }
